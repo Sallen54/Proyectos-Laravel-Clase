a8b0eb9570e1d2c23ef7852fb760bad0cac99aac
Alejandro <alejandalmau@gmail.com>
2025-12-09 11:34:57 +0100

    combinar elementos en rutas

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -9,3 +9,22 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return 'Listado de posts';
 });
+
+#rutas sesion 2
+Route::get('fecha', function () {
+    return date("d/m/y h:i:s");
+});
+
+Route::get(
+    'saludo/{nombre?}/{id?}',
+    function ($nombre = "Invitado", $id = 0) {
+        return "Hola $nombre, tu código es el $id";
+    }
+)->where('nombre', "[A-Za-z]+")
+    ->where('id', "[0-9]+")
+    ->name('saludo');
+
+Route::get('contacto', function () {
+    return "Página de contacto";
+})->name('ruta_contacto');
+
