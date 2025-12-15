df92a1ad887cd381b516637c726b39847e3dd840
Alejandro <alejandalmau@gmail.com>
2025-12-15 13:00:38 +0100

    probando sesion 3

---
+++ blog/app/Http/Controllers/PruebaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $datos = array(...);
+        return view('miVista', compact('datos'));
+    }
+}
