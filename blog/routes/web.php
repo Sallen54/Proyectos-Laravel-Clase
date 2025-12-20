a0e456b188175a189812ed5d0df0c773b2027870
Alejandro <alejandalmauggmail.com>
2025-12-20 13:34:56 +0000

    Session4

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -7,9 +7,7 @@ Route::get('/', function () {
     return view('home');
 })->name('inicio');
 
-// Rutas de prueba primero
 Route::get('/posts/nuevoPrueba', [PostController::class, 'nuevoPrueba']);
 Route::get('/posts/editarPrueba/{id}', [PostController::class, 'editarPrueba']);
 
-// Después el resource
 Route::resource('posts', PostController::class);
